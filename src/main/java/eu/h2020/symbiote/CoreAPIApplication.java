fa123d237de8e5bef6cb859a00ca908141ccf68c
Mikolaj Dobski <mikolaj.dobski@man.poznan.pl>
2016-09-16 16:08:40 +0200

    updated coreApi to coreAPI by convention

--- src/main/java/eu/h2020/symbiote/CoreAPIApplication.java
+++ src/main/java/eu/h2020/symbiote/CoreAPIApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreApiApplication {
+public class CoreAPIApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreApiApplication.class, args);
+		SpringApplication.run(CoreAPIApplication.class, args);
 	}
 }
