03149d7513f6ee5029ff38318c6285ac923d4806
Artur Jaworski <arturj@man.poznan.pl>
2016-09-16 13:59:44 +0200

    Changed project name

--- src/main/java/eu/h2020/symbiote/CoreApiApplication.java
+++ src/main/java/eu/h2020/symbiote/CoreApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreApiClientApplication {
+public class CoreApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreApiClientApplication.class, args);
+		SpringApplication.run(CoreApiApplication.class, args);
 	}
 }
